f2d9fee8d3ce99e702f8b312b784f2e4847d080c
Syed Awais Shah <22pwele5987@uetpeshawar.edu.pk>
2026-03-20 14:44:12 +0500

    Expose supabaseClient to the global window object

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 // This line is what creates the "function" .from()
 // In config.js
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
