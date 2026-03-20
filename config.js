f7ee3052086147bbb1394fe6a78289492a28cc2d
Syed Awais Shah <22pwele5987@uetpeshawar.edu.pk>
2026-03-20 13:48:18 +0500

    Update SUPABASE_ANON_KEY in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://nbmvodklvnmnuntbmvvb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // your full key
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ibXZvZGtsdm5tbnVudGJtdnZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5OTA1ODcsImV4cCI6MjA4OTU2NjU4N30.JcpIlozE-J2DmcCExeNWt0pgtDBL70VX7jAXOBnix6E'; // your full key
 
 // This line is what creates the "function" .from()
 // In config.js
